96daac2d69ed722b49245378c2b19106e88e08e2
Laura <ldiasvitaltoledo8@gmail.com>
2025-09-26 22:57:06 -0300

    feat: Correção de lógica

--- exercicio11_idep.py
+++ exercicio11_idep.py
@@ -12,17 +12,15 @@ cursor= conexao.cursor()
 print("=/" * 50)
 print("11) Encontre o ano que teve o maior valor para o ideb")
 
-value_year = "SELECT ano, ideb FROM ideb ORDER BY ideb DESC LIMIT 1"  
-# pega apenas o ano e o valor da coluna ideb e ordena em ordem decrescente pegando somente a primeira linha.
+value_year = "SELECT ano, ideb FROM ideb WHERE ideb = (SELECT MAX(ideb) FROM ideb);"  
+# pega apenas o ano e o valor da tabela ideb .
 
 cursor.execute(value_year)
-maior = 0
-registros = cursor.fetchone()
 
-year = registros[0]
-greater_value = registros[1]
+registros = cursor.fetchall()
 
-print(f"O ano em que teve o maior valor para o ideb foi:",year ,"e o valor foi:", greater_value)
+for reg in registros:
+  print(f"O ano em que teve o maior valor para o ideb foi:",reg)
 print("=" * 50) 
 
 conexao.close()
